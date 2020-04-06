65edca821bb9e0718d455f79138f62eb41705086
AlexChang124 <62864420+AlexChang124@users.noreply.github.com>
2020-04-06 14:16:59 -0400

    Create PieceC4.cpp

---
+++ PieceC4.cpp
@@ -0,0 +1,16 @@
+// Alex Chang, 400268761
+// PieceC4.cpp
+
+#include "PieceC4.h"
+
+PieceC4::PieceC4() {
+	piece = 0;
+}
+
+int PieceC4::getPiece() const {
+	return piece;
+}
+
+void PieceC4::setPiece(int p) {
+	piece = p;
+}
